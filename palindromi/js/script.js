e756f9c7c99bd15c70780f1189caf709b7d93cb5
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-18 17:41:13 +0200

    pal

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -1,26 +1,17 @@
 const container = document.querySelector(".container");
 container.classList.add("text-center");
 
+const btnVerifica = document.getElementById("btn_verifica");
+const word = document.getElementById("parola");
 
-
-
-const btnVerifica = document.getElementById("#btn_verifica");
-
-
+let parolaInvertita;
 
 btnVerifica.addEventListener("click", function () {
-    const word = document.querySelector("[name='word']");
+
     const parola = word.value;
-    console.log(word.value);
-
-    console.log("mia parola: " + parola);
-
-    console.log(word.value);
-
 
     function controlloPalindromo(parola) {
 
-        //Scompongo la parola nelle lettere che la compongono
         const parolaDivisa = parola.split("");
         console.log("parola split: " + parola);
 
@@ -35,14 +26,10 @@ btnVerifica.addEventListener("click", function () {
         return parolaJoin;
     }
 
-    const parolaInvertita = controlloPalindromo(parola);
+    parolaInvertita = controlloPalindromo(parola);
+    if (parola == parolaInvertita) {
+        alert(`La parola ${parola} è palindroma!`);
+    } else {
+        alert(`La parola ${parola} non è palindroma!`);
+    }
 });
-
-
-
-//controllo
-if (parola == parolaInvertita) {
-    alert(`La parola ${parola} è palindroma!`);
-} else {
-    alert(`La parola ${parola} non è palindroma!`);
-}
