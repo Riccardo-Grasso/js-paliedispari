fa35576eebdae04b8ce6d6c05f65958141ab519f
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-18 17:48:25 +0200

    palindromi_finito

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -1,19 +1,26 @@
 const container = document.querySelector(".container");
 container.classList.add("text-center");
 
+
+//Indico il mio BTN
 const btnVerifica = document.getElementById("btn_verifica");
 const word = document.getElementById("parola");
 
 let parolaInvertita;
 
-btnVerifica.addEventListener("click", function () {
+//Evento click
+btnVerifica.addEventListener("click", funzionePalindromi);
+
+//Funzione esterna, riclabile
+function funzionePalindromi() {
 
     const parola = word.value;
-
+    console.log("Parola iniziale: " + parola);
     function controlloPalindromo(parola) {
 
+        //divido la parola in lettere
         const parolaDivisa = parola.split("");
-        console.log("parola split: " + parola);
+        console.log("parola split: " + parolaDivisa);
 
         //Inverto l'array che contiene le lettere
         const parolaReverse = parolaDivisa.reverse();
@@ -32,4 +39,4 @@ btnVerifica.addEventListener("click", function () {
     } else {
         alert(`La parola ${parola} non è palindroma!`);
     }
-});
+}
